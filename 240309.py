da0ec9f522dfa129fe9198e6efcff7064291cd45
pilyeooong <pilyeooong@gmail.com>
2024-03-09 13:34:28 +0900

    add. pb solved

---
+++ 240309.py
@@ -0,0 +1,76 @@
+import heapq
+from collections import deque
+
+graph = {
+    1: [(2, 2), (1, 4)],
+    2: [(1, 3), (9, 5), (6, 6)],
+    3: [(4, 6)],
+    4: [(3, 3), (5, 7)],
+    5: [(1, 8)],
+    6: [(3, 5)],
+    7: [(7, 6), (9, 8)],
+    8: [],
+}
+
+
+def dijkstra(graph, start, final):
+    costs = {}
+    pq = []
+    heapq.heappush(pq, (0, start))
+
+    while pq:
+        cur_cost, cur_v = heapq.heappop(pq)
+        if cur_v not in costs:
+            costs[cur_v] = cur_cost
+            for cost, next_v in graph[cur_v]:
+                next_cost = cost + cur_cost
+                heapq.heappush(pq, (next_cost, next_v))
+    return costs[final]
+
+
+print(dijkstra(graph, 1, 8))
+
+s = "babad"
+
+
+def longest_palindrome(s):
+    if len(s) < 2 or s == s[::-1]:
+        return s
+
+    def expand(left, right):
+        while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
+            left += 1
+            right -= 1
+        return s[left - 1 : right + 1]
+
+    result = ""
+    for i in range(len(s) - 1):
+        result = max(result, expand(i, i + 1), expand(i, i + 2))
+
+    return result
+
+
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+
+
+def trapping_rain_water(height):
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right]
+    weight = 0
+
+    while left < right:
+        left_max = max(left_max, height[left])
+        right_max = max(right_max, height[right])
+
+        if left_max <= right_max:
+            weight += left_max - height[left]
+            left += 1
+        else:
+            weight += right_max - height[right]
+            right -= 1
+
+    return weight
+
+
+print(longest_palindrome(s))
+print(trapping_rain_water(height))
