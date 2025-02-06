528b0f6c5db6f2bc73ddd36bba1514726d38dcad
pilyeooong <pilyeooong@gmail.com>
2025-02-06 11:43:04 +0900

    add. daily

---
+++ daily/250102.py
@@ -0,0 +1,146 @@
+import heapq
+from collections import deque
+
+ramen_stock = 4
+supply_dates = [4, 10, 15]
+supply_supplies = [20, 5, 10]
+supply_recover_k = 30
+
+
+def get_minimum_count_of_overseas_supply(stock, dates, supplies, k):
+    last_idx = 0
+    answer = 0
+    heap_supplies = []
+
+    while stock <= k:
+        while last_idx < len(dates) and dates[last_idx] <= stock:
+            supply = supplies[last_idx]
+            heapq.heappush(heap_supplies, -supply)
+            last_idx += 1
+
+        heap_supply = heapq.heappop(heap_supplies)
+        stock += -heap_supply
+        answer += 1
+
+    return answer
+
+
+print(get_minimum_count_of_overseas_supply(ramen_stock, supply_dates, supply_supplies, supply_recover_k))
+print("정답 = 2 / 현재 풀이 값 = ", get_minimum_count_of_overseas_supply(4, [4, 10, 15], [20, 5, 10], 30))
+print("정답 = 4 / 현재 풀이 값 = ", get_minimum_count_of_overseas_supply(4, [4, 10, 15, 20], [20, 5, 10, 5], 40))
+print("정답 = 1 / 현재 풀이 값 = ", get_minimum_count_of_overseas_supply(2, [1, 10], [10, 100], 11))
+
+current_r, current_c, current_d = 7, 4, 0
+current_room_map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+
+def get_count_of_departments_cleaned_by_robot_vacuum(r, c, d, room_map):
+    answer = 0
+
+    row = len(room_map)
+    col = len(room_map[0])
+
+    visited = [[False] * col for _ in range(row)]
+
+    q = deque()
+    q.append((r, c, d))
+    visited[r][c] = True
+    answer += 1
+
+    # 회전 시 변경 방향
+    turn = {
+        0: 3, # 북
+        1: 0, # 동
+        2: 1, # 서
+        3: 2  # 남
+    }
+
+    # 후진 시 변경 방향
+    back = {
+        0: 2,
+        1: 3,
+        2: 0,
+        3: 1
+    }
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+    while q:
+        x, y, cur_d = q.popleft()
+        for i in range(4):
+            cur_d = turn[cur_d]
+            dx, dy = directions[cur_d]
+            next_x, next_y = x + dx, y + dy
+
+            if next_x >= 0 and next_x < row and next_y >= 0 and next_y < col and visited[next_x][next_y] == False and room_map[next_x][next_y] == 0:
+                q.append((next_x, next_y, cur_d))
+                visited[next_x][next_y] = True
+                answer += 1
+                break
+            elif i == 3:
+                back_d = back[cur_d]
+                dx, dy = directions[back_d]
+                back_x, back_y = x + dx, y + dy
+                q.append((back_x, back_y, cur_d))
+
+                if room_map[back_x][back_y] == 1:
+                    return answer
+
+    return answer
+
+
+# 57 가 출력되어야 합니다!
+print(get_count_of_departments_cleaned_by_robot_vacuum(current_r, current_c, current_d, current_room_map))
+current_room_map2 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 29 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6,3,1,current_room_map2))
+current_room_map3 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 33 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(7,4,1,current_room_map3))
+current_room_map4 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 25 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6,2,0,current_room_map4))
